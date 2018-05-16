28f5c6d35163d96f96c246c47a107ed0b07ba1aa
Dmitry Bilunov <kmeaw@yandex-team.ru>
2018-05-16 11:37:45 +0300

    Fix to match recent markup and make it possible to diff CPUs

--- arksearch/arksearch.py
+++ arksearch/arksearch.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 #
 # Copyright 2016 Major Hayden
+# Copyright 2017 kmeaw
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -20,13 +21,10 @@ Searches Intel's ARK site and returns data about various processors.
 TOTALLY UNOFFICIAL. ;)
 """
 from bs4 import BeautifulSoup
-
+import HTMLParser
 
 import click
-
-
 import requests
-
 
 from terminaltables import AsciiTable
 
@@ -49,66 +47,82 @@ def get_cpu_html(quickurl):
     return r.text
 
 
-def generate_table_data(html_output):
+def generate_table_data(*html_outputs):
     """Generate an ASCII table based on the HTML provided."""
-    soup = BeautifulSoup(html_output, 'html.parser')
 
     table_data = [
-        ['Parameter', 'Value']
     ]
 
-    for table in soup.select('table.specs'):
-        rows = table.find_all("tr")
-        for row in rows[1:]:
-            cells = [cell.get_text("\n", strip=True)
-                     for cell in row.find_all('td')]
+    keys = {}
 
-            if cells[0] == 'T\nCASE':
-                cells[0] = 'T(CASE)'
-            if "\n" in cells[0]:
-                cells[0] = cells[0][:cells[0].index("\n")]
+    hp = HTMLParser.HTMLParser()
 
-            table_data.append(cells)
+    for i, html_output in enumerate(html_outputs):
+        soup = BeautifulSoup(html_output, 'html.parser')
+        for table in soup.select('ul.specs-list'):
+            rows = table.find_all("li")
+            for row in rows:
+                key = row.find('span', class_='label').get_text("\n", strip=True)
+                value = hp.unescape(row.find('span', class_='value').get_text("\n", strip=True))
+
+                if key == 'T\nCASE':
+                    key = 'T(CASE)'
+                if "\n" in key:
+                    key = key[:key.index("\n")]
+
+                if key not in keys:
+                    keys[key] = len(keys)
+                    table_data.append([key] + [None] * len(html_outputs))
+                table_data[keys[key]][i + 1] = value
+
+    if len(html_outputs) > 1:
+        table_data = filter(lambda row: len(set(row[1:])) > 1, table_data)
+    table_data.insert(0, ['Parameter'] + ['Value'] * len(html_outputs))
 
     return table_data
 
 
-def quick_search(search_term):
-    url = "http://ark.intel.com/search/AutoComplete?term={0}"
-    headers = {
-        'User-Agent': USER_AGENT,
-    }
-    r = requests.get(url.format(search_term, headers=headers))
-    return r.json()
+def quick_search(*search_terms):
+    result = []
+    for search_term in search_terms:
+        url = "http://ark.intel.com/search/AutoComplete?term={0}"
+        headers = {
+            'User-Agent': USER_AGENT,
+        }
+        r = requests.get(url.format(search_term, headers=headers))
+        result.append(r.json())
+    return result
 
 
 @click.command()
-@click.argument('search_term')
+@click.argument('search_terms', nargs=-1)
 @click.pass_context
-def search(ctx, search_term):
+def search(ctx, search_terms):
     """Main function of the script."""
-    ark_json = quick_search(search_term)
+    ark_jsons = quick_search(*search_terms)
+    cpu_data = []
 
-    if len(ark_json) < 1:
-        click.echo("Couldn't find any processors matching "
-                   "{0}".format(search_term))
-        ctx.exit(0)
+    for ark_json, search_term in zip(ark_jsons, search_terms):
+        if len(ark_json) < 1:
+            click.echo("Couldn't find any processors matching "
+                       "{0}".format(search_term))
+            ctx.exit(0)
 
-    click.echo(u"Processors found: {0}".format(len(ark_json)))
-    choice_dict = {}
-    counter = 0
-    for cpu in ark_json:
-        choice_dict[counter] = cpu['quickUrl']
-        click.echo(u"[{0}] {1}".format(counter, cpu['value']))
-        counter += 1
+        click.echo(u"Processors found: {0}".format(len(ark_json)))
+        choice_dict = {}
+        counter = 0
+        for cpu in ark_json:
+            choice_dict[counter] = cpu['quickUrl']
+            click.echo(u"[{0}] {1}".format(counter, cpu['value']))
+            counter += 1
+        if counter == 1:
+            choice = 0
+        else:
+            choice = click.prompt(u"Which processor", prompt_suffix='? ', type=int)
 
-    if len(ark_json) > 1:
-        choice = click.prompt(u"Which processor", prompt_suffix='? ', type=int)
-    else:
-        choice = 0
+        cpu_data.append(get_cpu_html(choice_dict[int(choice)]))
 
-    cpu_data = get_cpu_html(choice_dict[int(choice)])
-    table_data = generate_table_data(cpu_data)
+    table_data = generate_table_data(*cpu_data)
     table = AsciiTable(table_data)
     click.echo(table.table)
     ctx.exit(0)
